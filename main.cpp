b44c31ece95e8d6e250d1b6025f02ddfaa49ea53
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-10-27 20:30:19 -0400

    2d array flattened to 1d

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <fstream>
 #include <cmath>
 #include <chrono>
+#include <sstream>
 #include <cstdlib>
 #include <map>
 #include <vector>
 #include <random>
 #include <algorithm>
 #include <cfloat>
+#include <sys/time.h>
 
 using namespace std;
 int main (int argc, char *argv[]) {
@@ -22,10 +24,8 @@ int main (int argc, char *argv[]) {
     int imageSize = atoi(argv[1]);
     int numPoints = atoi(argv[2]);
 
-    // create 2D array to hold the calculation results
-    int** imageArray = new int*[imageSize];
-    for(int i = 0; i < imageSize; i++)
-        imageArray[i] = new int[imageSize];
+    // create array to hold the calculation results
+    int* imageArray = new int[imageSize * imageSize];
 
     // create vectors of possible X and Y coordinates for the points
     vector<int> pointsX;
@@ -44,9 +44,11 @@ int main (int argc, char *argv[]) {
     shuffle(pointsY.begin(), pointsY.end(), default_random_engine(seedY));
 
     // fill the points with random coordinates
-    int** points = new int*[numPoints];
-    for(int i = 0; i < numPoints; i++)
-        points[i] = new int[2] {pointsX[i], pointsY[i]};
+    int* points = new int[numPoints + numPoints];
+    for(int i = 0; i < numPoints; i++) {
+        points[i] = pointsX[i];
+        points[i + numPoints] = pointsY[i];
+    }
 
     // create maps to hold the RGB values of each point
     map<int, int> pointRed;
@@ -65,29 +67,36 @@ int main (int argc, char *argv[]) {
         pointBlue[i] = rand() % 256;
     }
 
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
     // loop through each cell, compare to each point and assign cell to closest point
-    for (int i=0; i<imageSize; i++) {
-        for (int j=0; j<imageSize; j++) {
-            double minDistance = DBL_MAX;
-            int minPoint = -1;
-            for (int k=0; k<numPoints; k++) {
-                double distance = sqrt(pow(j - points[k][1], 2) + pow(i - points[k][0], 2));
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    minPoint = k;
-                }
+    for (int x=0; x<imageSize * imageSize; x++) {
+        double minDistance = DBL_MAX;
+        int minPoint = -1;
+        for (int k=0; k<numPoints; k++) {
+            double distance = sqrt(pow((double) (x % imageSize - points[k + numPoints]), 2.0) + pow((double) (x / imageSize - points[k]), 2.0));
+            if (distance < minDistance) {
+                minDistance = distance;
+                minPoint = k;
             }
-            imageArray[i][j] = minPoint;
         }
+        imageArray[x] = minPoint;
     }
+
+    gettimeofday(&end, NULL);
+
+    long seconds = (end.tv_sec - start.tv_sec);
+    long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+    printf("Processing time elpased is %zu seconds or %zu micros\n", seconds, micros);
 
     // loop through points and set color to white (numPoints)
     for (int i=0; i<numPoints; i++) {
-        imageArray[points[i][0]][points[i][1]] = numPoints;
+        imageArray[points[i + numPoints] + imageSize * points[i]] = numPoints;
     }
-
-    // write points to file by pulling colors from maps
-    ofstream output("C:/Users/jeffp/CLionProjects/HPC3/output.ppm", ios_base::binary);
+    
+    ofstream output("output.ppm", ios_base::binary);
 
     // store the image header in our output file
     output << "P3" << "\n";
@@ -95,21 +104,15 @@ int main (int argc, char *argv[]) {
     output << "255" << "\r\n";
 
     // store the updated array in our output file
-    for(int row = 0; row < imageSize; ++row) {
-        for(int col = 0; col < imageSize; ++col) {
-            output << pointRed[imageArray[row][col]] << " " << pointGreen[imageArray[row][col]] << " " << pointBlue[imageArray[row][col]] << " ";
-        }
+    for(int i = 0; i < imageSize * imageSize; i++) {
+        output << pointRed[imageArray[i]] << " " << pointGreen[imageArray[i]] << " " << pointBlue[imageArray[i]] << " ";
         output << "\r\n";
     }
+
     output.close();
 
     // delete arrays from memory
-    for(int i = 0; i < imageSize; i++)
-        delete[] imageArray[i];
     delete[] imageArray;
-
-    for(int i = 0; i < numPoints; i++)
-        delete[] points[i];
     delete[] points;
 
     return 0;
